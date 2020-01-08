8983863105a586641db3450b1584e874a213bcdc
Bej <tanumon.bej@siemens-healthineers.com>
2020-01-08 14:58:05 +0530

    Update from Alt 2020-01-08_02-58-PM

--- SummuApp/controller.js
+++ SummuApp/controller.js
@@ -6,11 +6,13 @@ angular
         title:"Hd-Reg",
         total:0,
         entry:0,
+        count:0,
         history:[]
     },{
         title:"Ax-Flip",
         total:0,
         entry:0,
+        count:0,
         history:[]
     },{
         title:"Ic-Amz",
@@ -18,6 +20,17 @@ angular
         entry:0,
         history:[]
     }];
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+	function onDeviceReady() {
+		var storage = window.localStorage;
+		if(storage.getItem('objToStore')) {
+			var jsonObj = JSON.parse(storage.getItem('objToStore'));
+            $scope.ObjToStore = jsonObj;
+            $scope.$apply();
+		}
+    }
+    
     $scope.add = ()=>{
         angular.forEach($scope.ObjToStore,(value)=>{
             if(value.entry > 0){
@@ -27,7 +40,20 @@ angular
                 value.entry = 0;
             }
         });
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {       
+            var jsonStr = JSON.stringify($scope.ObjToStore);
+            var storage = window.localStorage;
+            storage.setItem('objToStore', jsonStr);
+        }
     };
+
+    $scope.incr = (val)=>{
+        val.count++;
+    }
+    $scope.decr = (val)=>{
+        val.count--;
+    }
     $scope.returnClass = (obj)=>{
         let id = $scope.ObjToStore.findIndex(element=>element===obj); 
         if((id === 0 && obj.total > 300000) || (id === 1 && obj.total > 200000)){
